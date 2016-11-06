c5e2630f4cbf51037f7936e9662b1ebdd60b520e
Quentin Claudel <quentin.claudel@me.com>
2016-11-07 00:56:12 +0100

    Add a preliminary version of register method

--- app/src/Controllers/UserController.php
+++ app/src/Controllers/UserController.php
@@ -27,4 +27,55 @@ final class UserController
 
 		return $this->view->render($response, 'users.twig', ["data" => $users]);
     }
+    
+    /**
+     * Method which check form datas and register the user
+     */
+    public function registerUser(Request $request, Response $response, $args) {
+        $this->logger->info("Start to register a new user");
+        
+        //Get form datas and filter them
+        $data = $request->getParsedBody();
+        $userForm = array();
+        $userForm['email'] = filter_var($data['email'], FILTER_SANITIZE_STRING);
+        $userForm['password'] = filter_var($data['password'], FILTER_SANITIZE_STRING);
+        $userForm['passwordRepeat'] = filter_var($data['passwordRepeat'], FILTER_SANITIZE_STRING);
+        $userForm['first_name'] = filter_var($data['first_name'], FILTER_SANITIZE_STRING);
+        $userForm['last_name'] = filter_var($data['last_name'], FILTER_SANITIZE_STRING);
+        $userForm['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
+        
+        if($userForm['password'] == $userForm['passwordRepeat']) {
+            if(filter_var($userForm['email'], FILTER_VALIDATE_EMAIL, FILTER_FLAG_PATH_REQUIRED)) {
+                $credentials = [
+                    'email' => $userForm['email'],
+                ];
+                
+                if(User::where('email', $userForm['email'])->get()->count() > 0) {
+                    if(!empty($userForm['first_name']) && !empty($userForm['last_name'])) {
+                        //Successful verification
+                        Sentinel::register($userForm);
+                        
+                        return $this->view->render($response, 'register.twig', array(   'success' => true, 
+                                                                                        'message' => 'Bravo '.$userForm['first_name'].' ! Vous avez bien été enregistré.'));
+                    } else {
+                        $this->logger->info('Error: first_name and last_name can\'t be empty');
+                        return $this->view->render($response, 'register.twig', array(   'success' => false, 
+                                                                                        'message' => 'Vous devez remplir les champs "nom" et "prénom" pour valider votre inscription.'));
+                    }
+                } else {
+                    $this->logger->info('Error: there is already a user using the given email address');
+                    return $this->view->render($response, 'register.twig', array(   'success' => false, 
+                                                                                        'message' => 'Un utilisateur utilise déjà cette adresse e-mail.'));
+                }
+            } else {
+                $this->logger->info('Error: the given email address wasn\'t valid');
+                return $this->view->render($response, 'register.twig', array(   'success' => false, 
+                                                                                        'message' => 'L\'adresse e-mail que vous avez fournie n\'est pas valide.'));
+            }
+        } else {
+            $this->logger->info('Error: password and passwordRepeat didn\'t match');
+            return $this->view->render($response, 'register.twig', array(   'success' => false, 
+                                                                                        'message' => 'Les deux mots de passe que vous avez entrez ne correspondent pas.'));
+        }
+    }
 }
