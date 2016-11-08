b663c947ab23461e76f2715090bd0b293a2981d0
Quentin Claudel <quentin.claudel@me.com>
2016-11-08 17:39:21 +0100

    Add route and controller to display user's profiles

---
+++ app/src/Controllers/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Log\LoggerInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+
+use App\Models\User;
+
+final class ProfileController
+{
+    private $view;
+    private $logger;
+	private $user;
+
+    public function __construct($view, LoggerInterface $logger, $user)
+    {
+        $this->view = $view;
+        $this->logger = $logger;
+        $this->model = $user;
+    }
+
+    /**
+     * Method which display user profile from its id 
+     * args - {id: user's id}
+     */
+    public function displayProfile(Request $request, Response $response, $args) {
+
+    }
+}
