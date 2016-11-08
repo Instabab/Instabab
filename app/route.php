13df381ad7662fd6107c1f9be02f68688968f57e
Quentin Claudel <quentin.claudel@me.com>
2016-11-08 14:37:32 +0100

    Add 'no authentication' middleware to login and register routes

--- app/route.php
+++ app/route.php
@@ -4,8 +4,15 @@
 	controller needs to be registered in dependency.php
 */
 
+//Routes opened to every user
 $app->get('/', 'App\Controllers\HomeController:displayHomepage')->setName('homepage');
+
+//Routes restricted to conencted users
+
+//Routes where the user has to be disconnected
+$app->post('/register', 'App\Controllers\UserController:registerUser')->setName('register')->add('checkNoAuthentication');
+$app->post('/login', 'App\Controllers\UserController:login')->setName('login')->add('checkNoAuthentication');
+
+//Route redirections to restricted access pages
 $app->get('/need/authentication', 'App\Controllers\UserController:userNeedsToAuthenticate')->setName('needAuthentication');
 $app->get('/need/logout', 'App\Controllers\UserController:userNeedsToLogout')->setName('needLogout');
-$app->post('/register', 'App\Controllers\UserController:registerUser')->setName('register');
-$app->post('/login', 'App\Controllers\UserController:login')->setName('login');
